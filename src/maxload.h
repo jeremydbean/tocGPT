087d5b0b5ed26247df1ef3a6a78386ef64465323
Jeremy Bean <beanj007@gmail.com>
2025-11-18 03:41:07 -0500

    Fix maxload header for Docker build

--- src/maxload.h
+++ src/maxload.h
@@ -1,6 +1,18 @@
 /*
  * Values mirrored from db.c and used across the maxload implementation.
  */
+#ifndef MAXLOAD_H
+#define MAXLOAD_H
+
+/* Provide the ITEM_MAX_LOAD forward declaration and args() macro when this
+ * header is included before merc.h.
+ */
+#ifndef args
+#define args(list) list
+#endif
+
+typedef struct item_max_load ITEM_MAX_LOAD;
+
 #define MAXLOAD_KEY_HASH 127
 #define MAXLOADFILE "../area/maxload.txt"
 
@@ -10,3 +22,5 @@ extern int top_maxload;
 
 /* Hook used after loading the maxload file. */
 int maxload_chain args((void));
+
+#endif /* MAXLOAD_H */
