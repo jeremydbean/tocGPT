645f383b74b190b92fb13d4031c99d798ae6112a
Jeremy Bean <beanj007@gmail.com>
2025-11-18 03:35:08 -0500

    Format maxload code and document behavior

--- src/maxload.h
+++ src/maxload.h
@@ -1,6 +1,12 @@
-/* vals from db.c */
+/*
+ * Values mirrored from db.c and used across the maxload implementation.
+ */
 #define MAXLOAD_KEY_HASH 127
 #define MAXLOADFILE "../area/maxload.txt"
-extern ITEM_MAX_LOAD    * maxload_index_hash      [MAXLOAD_KEY_HASH];
-extern int                top_maxload;
-       int                maxload_chain        args( (void) );
+
+/* Global hash table and counter defined in maxload.c. */
+extern ITEM_MAX_LOAD *maxload_index_hash[MAXLOAD_KEY_HASH];
+extern int top_maxload;
+
+/* Hook used after loading the maxload file. */
+int maxload_chain args((void));
